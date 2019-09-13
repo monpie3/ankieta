9f66f71bc206b66f37cb48b7b3c1b030796b8f9b
MonikaPierzchala <dotyk_zla@o2.pl>
2019-09-13 07:27:47 +0200

    Pytania zrobione przez Asiek

--- unit5_webapp.py
+++ unit5_webapp.py
@@ -5,31 +5,56 @@ import statistics
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///formdata.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///formdata.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
 
 db = SQLAlchemy(app)
+
 
 class Formdata(db.Model):
     __tablename__ = 'formdata'
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.now)
-    firstname = db.Column(db.String, nullable=False)
-    email = db.Column(db.String)
+    gender = db.Column(db.Integer)
     age = db.Column(db.Integer)
-    income = db.Column(db.Integer)
-    satisfaction = db.Column(db.Integer)
-    q1 = db.Column(db.Integer)
+    education = db.Column(db.Integer)
+    location = db.Column(db.Integer)
+    voting = db.Column(db.Integer)
+    g1 = db.Column(db.Integer)
     q2 = db.Column(db.Integer)
+    q3 = db.Column(db.Integer)
+    g4 = db.Column(db.Integer)
+    q5 = db.Column(db.Integer)
+    q6 = db.Column(db.Integer)
+    g7 = db.Column(db.Integer)
+    q8 = db.Column(db.Integer)
+    q9 = db.Column(db.Integer)
+    g10 = db.Column(db.Integer)
+    q11 = db.Column(db.Integer)
+    q12 = db.Column(db.Integer)
+    q13 = db.Column(db.Integer)
 
-    def __init__(self, firstname, email, age, income, satisfaction, q1, q2):
-        self.firstname = firstname
-        self.email = email
+    def __init__(self, gender, age, education, location, voting, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12,
+                 q13):
+        self.gender = gender
         self.age = age
-        self.income = income
-        self.satisfaction = satisfaction
+        self.education = education
+        self.location = location
+        self.voting = voting
         self.q1 = q1
         self.q2 = q2
+        self.q3 = q3
+        self.q4 = q4
+        self.q5 = q5
+        self.q6 = q6
+        self.q7 = q7
+        self.q8 = q8
+        self.q9 = q9
+        self.q10 = q10
+        self.q11 = q11
+        self.q12 = q12
+        self.q13 = q13
+
 
 db.create_all()
 
@@ -38,13 +63,16 @@ db.create_all()
 def welcome():
     return render_template('welcome.html')
 
+
 @app.route("/team")
 def show_team():
     return render_template('team.html')
 
+
 @app.route("/form")
 def show_form():
     return render_template('form.html')
+
 
 @app.route("/raw")
 def show_raw():
@@ -89,16 +117,27 @@ def show_result():
 @app.route("/save", methods=['POST'])
 def save():
     # Get data from FORM
-    firstname = request.form['firstname']
-    email = request.form['email']
+    gender = request.form['gender']
     age = request.form['age']
-    income = request.form['income']
-    satisfaction = request.form['satisfaction']
+    education = request.form['education']
+    location = request.form['location']
+    voting = request.form['voting']
     q1 = request.form['q1']
     q2 = request.form['q2']
+    q3 = request.form['q3']
+    q4 = request.form['q4']
+    q5 = request.form['q5']
+    q6 = request.form['q6']
+    q7 = request.form['q7']
+    q8 = request.form['q8']
+    q9 = request.form['q9']
+    q10 = request.form['q10']
+    q11 = request.form['q11']
+    q12 = request.form['q12']
+    q13 = request.form['q13']
 
     # Save the data
-    fd = Formdata(firstname, email, age, income, satisfaction, q1, q2)
+    fd = Formdata(gender, age, education, location, voting, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13)
     db.session.add(fd)
     db.session.commit()
 
