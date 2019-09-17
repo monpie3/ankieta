aecf344d1cec3e46fadb141efd9420fc95cba1f4
MonikaPierzchala <dotyk_zla@o2.pl>
2019-09-17 21:52:01 +0200

    chyba działa

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'zyciejestnobelon'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProductionConfig(Config):
