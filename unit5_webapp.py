e09d0d65f3a5dd5d6cb056269f9422630e49de62
MonikaPierzchala <dotyk_zla@o2.pl>
2019-09-17 21:38:25 +0200

    ten sam błąd lokalnie co na heroku

--- unit5_webapp.py
+++ unit5_webapp.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from dotenv import load_dotenv
 import statistics
 import os
 
+load_dotenv('./.env', verbose=True)
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///formdata.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'FALSE'
 
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'FALSE'
 db = SQLAlchemy(app)
 
 
